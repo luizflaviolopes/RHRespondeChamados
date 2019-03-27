639a8da5bb1b754477c23636c22bdf97b168d29c
Renato Tiago Nascimento O. Junio (SEPLAG) <x18490417@ca.intra>
2019-03-27 10:08:43 -0300

    teste menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import './css/bootstrap.css';
+import Menu from 'Menu.js'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="container">
         
       </div>
     );
