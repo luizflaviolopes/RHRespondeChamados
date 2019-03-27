5b252ef39695f88a68e190ffbde539750772e0c0
Cláudio Alex de Oliveira (SEPLAG) <M40490@ca.intra>
2019-03-27 14:49:23 -0300

    chamado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
+import Chamado from './Chamado.js';
 import logo from './logo.svg';
 import './App.css';
 
+var demandas = [{nome:'1', assunto:'pagamnento',mensagem:'quando serei pago?', atendente:'nº1'},
+{nome:'2', assunto:'férias',mensagem:'minhas férias não estão no sistema', atendente:'nº2'},
+{nome:'3', assunto:'licença',mensagem:'posso sair de çlicença novamente?', atendente:'nº3'},
+{nome:'4', assunto:'teste',mensagem:'ops', atendente:'nº1'},]
+
 class App extends Component {
+
+
+
+  constructor(props){
+    super(props);
+    this.state = {dems:demandas};
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <table>
+          <tbody>
+          {this.state.dems.map(function(a,i){
+            return (<Chamado nome={a.nome} assunto={a.assunto} mensagem={a.mensagem} atendente={a.atendente} /> )
+
+          })}
+          </tbody>
+        </table>
       </div>
     );
   }
---
+++ src/Chamado.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+
+const td = {
+    display: 'tableCell',
+    border: 'solid black 1px'
+
+}
+
+
+class Chamado extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {nome: props.nome ,assunto:props.assunto,mensagem:props.mensagem, atendente:props.atendente};
+        this.OnclickHande = this.OnclickHande.bind(this);
+      }
+
+      OnclickHande(){
+        console.log('clicado na linha de nome '+ this.state.nome)
+      }
+
+
+
+  render() {
+    return (
+      <tr onClick={this.OnclickHande}>
+          <td style={td}>{this.state.assunto}</td>
+          <td style={td}>{this.state.mensagem}</td>
+          <td style={td}>{this.state.atendente}</td>
+      </tr>
+    );
+  }
+}
+
+export default Chamado;
