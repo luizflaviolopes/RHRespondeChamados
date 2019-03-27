dab026727240f68ce7431d42e85e6849d3f4c22b
Cláudio Alex de Oliveira (SEPLAG) <M40490@ca.intra>
2019-03-27 15:30:52 -0300

    Claudio

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import Chamado from './Chamado.js';
-import logo from './logo.svg';
+
 import './App.css';
 
-var demandas = [{nome:'1', assunto:'pagamnento',mensagem:'quando serei pago?', atendente:'nº1'},
-{nome:'2', assunto:'férias',mensagem:'minhas férias não estão no sistema', atendente:'nº2'},
-{nome:'3', assunto:'licença',mensagem:'posso sair de çlicença novamente?', atendente:'nº3'},
-{nome:'4', assunto:'teste',mensagem:'ops', atendente:'nº1'},]
+var demandas = [{chamado:'1', nome:'1', assunto:'pagamnento',mensagem:'quando serei pago?'},
+{nome:'2', assunto:'férias',mensagem:'minhas férias não estão no sistema', chamado:'2'},
+{nome:'3', assunto:'licença',mensagem:'posso sair de çlicença novamente?', chamado:'3'},
+{nome:'4', assunto:'teste',mensagem:'ops', chamado:'4'},]
 
 class App extends Component {
 
@@ -23,7 +23,7 @@ class App extends Component {
         <table>
           <tbody>
           {this.state.dems.map(function(a,i){
-            return (<Chamado nome={a.nome} assunto={a.assunto} mensagem={a.mensagem} atendente={a.atendente} /> )
+            return (<Chamado nome={a.nome} assunto={a.assunto} mensagem={a.mensagem} atendente={a.chamado} /> )
 
           })}
           </tbody>
