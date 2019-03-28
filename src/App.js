e02965df610900a45358c2c2b818ae23733b7652
Cláudio Alex de Oliveira (SEPLAG) <M40490@ca.intra>
2019-03-28 11:40:54 -0300

    novo

--- src/App.js
+++ src/App.js
@@ -4,12 +4,6 @@ import Menu from './Menu.js';
 import './App.css';
 import './css/bootstrap.css';
 
-<<<<<<< HEAD
-var demandas = [{nome:'1', assunto:'pagamnento',mensagem:'quando serei pago?',protocolo:'1'},
-{nome:'2', assunto:'férias',mensagem:'minhas férias não estão no sistema', protocolo:'2'},
-{nome:'3', assunto:'licença',mensagem:'posso sair de licença novamente?', protocolo:'3'},
-{nome:'4', assunto:'teste',mensagem:'ops', protocolo:'4'},]
-=======
 import Botoes from './Botoes.js';
 
 var demandas = [{numChamado:'1', nome:'Renato', assunto:'Front',masp:'1321321',mensagem:'Consegue visualizar tudo'},
@@ -17,7 +11,6 @@ var demandas = [{numChamado:'1', nome:'Renato', assunto:'Front',masp:'1321321',m
 {numChamado:'3', nome:'Claudio', assunto:'Suporte',masp:'987654132',mensagem:'Consegue visualizar tudo'},
 {numChamado:'4', nome:'Luiz', assunto:'Full',masp:'564/654',mensagem:'Consegue visualizar tudo'},
 {numChamado:'5', nome:'Endrew', assunto:'DBA',masp:'312465',mensagem:'Consegue visualizar tudo'},]
->>>>>>> 0b172c05ce2baa9cb0710953f5f8114dc0694124
 
 class App extends Component {
 
--- src/Chamado.js
+++ src/Chamado.js
@@ -6,11 +6,7 @@ class Chamado extends Component {
 
     constructor(props){
         super(props);
-<<<<<<< HEAD
-        this.state = {nome: props.nome ,assunto:props.assunto,mensagem:props.mensagem, protocolo:props.protocolo};
-=======
         this.state = {numChamado: props.numChamado, nome: props.nome ,assunto:props.assunto,mensagem:props.mensagem, masp:props.masp};
->>>>>>> 0b172c05ce2baa9cb0710953f5f8114dc0694124
         this.OnclickHande = this.OnclickHande.bind(this);
       }
 
@@ -22,16 +18,10 @@ class Chamado extends Component {
   render() {
     return (
       <tr onClick={this.OnclickHande}>
-<<<<<<< HEAD
-          <td style={td}>{this.state.assunto}</td>
-          <td style={td}>{this.state.mensagem}</td>
-          <td style={td}>{this.state.protocolo}</td>
-=======
           <td >{this.state.nome}</td>
           <td >{this.state.numChamado}</td>
           <td >{this.state.assunto}</td>
           <td >{this.state.masp}</td>
->>>>>>> 0b172c05ce2baa9cb0710953f5f8114dc0694124
       </tr>
     );
   }
