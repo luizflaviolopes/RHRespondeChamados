7da2cd918b503ffc9b25b23a3de5fec6c7f01c03
Endrew Augusto Barbosa (SEPLAG) <x16676014@ca.intra>
2019-03-28 15:26:21 -0300

    Endrew

--- src/Chamado.js
+++ src/Chamado.js
@@ -6,22 +6,23 @@ class Chamado extends Component {
 
     constructor(props){
         super(props);
-        this.state = {numChamado: props.numChamado, nome: props.nome ,assunto:props.assunto,mensagem:props.mensagem, masp:props.masp};
+        this.state = {numChamado: props.numChamado, solicitante: props.solicitante ,assunto:props.assunto,data:props.data, status:props.status};
         this.OnclickHande = this.OnclickHande.bind(this);
       }
 
       OnclickHande(){
-        console.log('clicado na linha de nome '+ this.state.nome);
+        console.log('clicado na linha de chamado '+ this.state.numChamado);
         
       }
 
   render() {
     return (
       <tr onClick={this.OnclickHande}>
-          <td >{this.state.nome}</td>
           <td >{this.state.numChamado}</td>
+          <td >{this.state.solicitante}</td>        
           <td >{this.state.assunto}</td>
-          <td >{this.state.masp}</td>
+          <td >{this.state.data}</td>
+          <td >{this.state.status}</td>
       </tr>
     );
   }
