b80312c51a284adf0663d6af3b9b9a74437b870f
Renato Tiago Nascimento O. Junio (SEPLAG) <x18490417@ca.intra>
2019-04-10 15:10:29 -0300

    a

--- src/App.js
+++ src/App.js
@@ -15,8 +15,7 @@ import Row from 'react-bootstrap/Row';
 import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
 import '@trendmicro/react-sidenav/dist/react-sidenav.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {PageChamado} from './PageChamado.js';
-import Botoes from './Botoes.js';
+
 
 
 class App extends Component {
